315bb3a9b3a591262cc98cfcb1f572ae459da67d
sed45 <sed45@duke.edu>
2018-12-03 14:26:54 -0500

    initial commit

---
+++ src/runAll.R
@@ -0,0 +1,1 @@
+# Run all files in one batch
---
+++ src/wrangle.R
@@ -0,0 +1,9 @@
+library(dplyr)
+library(readr)
+library(fields)
+library(lme4)
+library(ggplot2)
+
+# Minimal Wrangling of the data
+gmpts <- bind_rows(gm182, gm182UP)
+colnames(gmpts) <- c('trackNum', 'time', 'longitude', 'latitude', 'status')
